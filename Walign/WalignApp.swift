6a45d67445488e3471ba012bdf2fe1d8825c43eb
Pranav Sharma <pranavs970@gmail.com>
2025-03-14 15:56:51 -0700

    Initial Commit

---
+++ Walign/WalignApp.swift
@@ -0,0 +1,17 @@
+//
+//  WalignApp.swift
+//  Walign
+//
+//  Created by Pranav Sharma on 2025-03-14.
+//
+
+import SwiftUI
+
+@main
+struct WalignApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
